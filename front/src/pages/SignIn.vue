072fe63e8fda0da12a4f3decddd520ca8e41a042
sarah <sar.brisson@gmail.com>
2020-10-26 16:42:54 +0100

    feat pages authentification

---
+++ front/src/pages/SignIn.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="content">
+      <auth-card :connexion="false"></auth-card>
+  </div>
+</template>
+<script>
+import AuthCard from 'src/components/Cards/AuthCard.vue'
+
+export default {
+  name: 'SignIn',
+  components: { AuthCard },
+
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+    
+  },
+};
+</script>
+<style>
+.content {
+  display: flex;
+  justify-content: center;
+  background-color: rgba(203, 203, 210, 0.15) !important;
+}
+</style>
---
+++ front/src/pages/SignUp.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="content">
+    <auth-card :connexion="true"></auth-card>
+  </div>
+</template>
+<script>
+import AuthCard from "src/components/Cards/AuthCard.vue";
+
+export default {
+  name: "SignUp",
+  components: { AuthCard },
+
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+<style>
+.content {
+  display: flex;
+  justify-content: center;
+  background: rgba(203, 203, 210, 0.15) !important;
+}
+</style>
