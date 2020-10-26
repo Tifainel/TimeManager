a798effa04d2a05431b9e32787f3cecc825761c5
sarah <sar.brisson@gmail.com>
2020-10-26 15:19:04 +0100

    feat workingtime page

---
+++ front/src/pages/WorkingTimes.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="content">
+    <div class="container-fluid">
+        <working-times-table :mini="false"></working-times-table>
+    </div>
+  </div>
+</template>
+<script>
+
+import Cookies from 'js-cookie';
+import jwt_decode from 'jwt-decode';
+import WorkingTimesTable from '../components/WorkingTimes/WorkingTimesTable'
+
+export default {
+  name: 'WorkingTimes',
+  components: { WorkingTimesTable },
+
+  data() {
+    return {
+      email: '',
+      username: '',
+      userId: '',
+    };
+  },
+  methods: {
+    
+  },
+
+  async mounted() {
+
+  },
+};
+</script>
+<style>
+.row {
+  display: flex;
+  justify-content: center;
+}
+</style>
