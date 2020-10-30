539fc2a1a4034d0527b6b122f0a6874a9fac9991
Aymerick <aymerick.domergue@epitech.eu>
2020-10-30 11:20:45 +0100

    (fix) Hash password when editing an user if password has changed. Cleaning the code

--- api/src/lib/src_web/controllers/user_controller.ex
+++ api/src/lib/src_web/controllers/user_controller.ex
@@ -13,12 +13,9 @@ defmodule SrcWeb.UserController do
 
   def create(conn, %{"user" => user_params}) do
     pwd = :crypto.hash(:md5, user_params["password"]) |> Base.encode16()
-    u = %{
-      "username"=>user_params["username"],
-      "email"=>user_params["email"],
-      "password"=> pwd
-    }
-    with {:ok, %User{} = user} <- Users.create_user(u) do
+    user_params = Map.put(user_params, "password", pwd)
+
+    with {:ok, %User{} = user} <- Users.create_user(user_params) do
       conn
       |> put_status(:created)
       |> put_resp_header("location", Routes.user_path(conn, :show, user))
@@ -59,15 +56,17 @@ defmodule SrcWeb.UserController do
 
   def update(conn, %{"id" => id, "user" => user_params}) do
     user = Users.get_user!(id)
-    pwd = :crypto.hash(:md5, user_params["password"]) |> Base.encode16()
-    u = %{
-      "username"=>user_params["username"],
-      "email"=>user_params["email"],
-      "password"=> pwd
-    }
-    with {:ok, %User{} = user} <- Users.update_user(user, u) do
+    current_password = Map.fetch!(user, :password)
+    user_params = if current_password !== user_params["password"] do
+      pwd = :crypto.hash(:md5, user_params["password"]) |> Base.encode16()
+      Map.put(user_params, "password", pwd)
+    else
+      user_params
+    end
+    with {:ok, %User{} = user} <- Users.update_user(user, user_params) do
       render(conn, "show.json", user: user)
     end
+
   end
 
   def delete(conn, %{"id" => id}) do
